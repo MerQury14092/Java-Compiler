e83b98e910576a13202509f5646cf8b02ef69d2b
MerQury14092 <petrakov14092@gmail.com>
2023-05-20 17:26:27 +0300

    approximate form of the future application

--- src/main/java/org/example/View/Menu.java
+++ src/main/java/org/example/View/Menu.java
@@ -5,20 +5,28 @@ import java.awt.*;
 import java.util.stream.IntStream;
 
 public class Menu extends JFrame {
+    private GridBagConstraints locator;
     public Menu(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
-        GridBagLayout grid = new GridBagLayout();
-        setLayout(grid);
+        GridBagLayout gridLayout = new GridBagLayout();
+        setLayout(gridLayout);
         int dotSize = 20;
-        grid.columnWidths = IntStream.generate(() -> dotSize).limit(16).toArray();
-        grid.rowHeights = IntStream.generate(() -> dotSize).limit(22).toArray();
+        gridLayout.columnWidths = IntStream.generate(() -> dotSize).limit(16).toArray();
+        gridLayout.rowHeights = IntStream.generate(() -> dotSize).limit(22).toArray();
         setTitle("JCompiler");
+
+        locator = new GridBagConstraints();
 
         JFileChooser fileChooser = new JFileChooser();
 
-        JLabel title = new JLabel("Java Compiler");
+        Color topBackgroundColor = new Color(0,255,0);
+        Color centerBackgroundColor = new Color(0,0,255);
+        Color bottomBackgroundColor = new Color(255,0,0);
+
         JLabel author = new JLabel("Created by MerQury");
+        JLabel title = new JLabel("Java Compiler");
+        title.setFont(new Font(author.getFont().getName(), Font.BOLD, 25));
         JTextArea urlToJar = new JTextArea("jar");
         JTextArea urlToExe = new JTextArea("exe");
         JTextArea urlToIcon = new JTextArea("ico");
@@ -26,19 +34,49 @@ public class Menu extends JFrame {
         JTextArea processName = new JTextArea("name");
 
         JCheckBox excludeCli = new JCheckBox("Exclude CLI");
+        excludeCli.setBackground(centerBackgroundColor);
+        excludeCli.setForeground(Color.WHITE);
         JCheckBox useCustomConfig = new JCheckBox("Use custom config\n(required for reflection)");
+
+        JPanel topBackground = new JPanel();
+        topBackground.setPreferredSize(new Dimension(16*dotSize, 4*dotSize));
+        topBackground.setBackground(topBackgroundColor);
+        JPanel centerBackground = new JPanel();
+        centerBackground.setPreferredSize(new Dimension(16*dotSize, 22*dotSize));
+        centerBackground.setBackground(centerBackgroundColor);
+        JPanel cmdBackground = new JPanel();
+        cmdBackground.setPreferredSize(new Dimension(16*dotSize, 4*dotSize));
+        cmdBackground.setBackground(bottomBackgroundColor);
+
 
         JButton compile = new JButton("Compile");
 
-        add(title);
-        add(urlToJar);
-        add(urlToExe);
-        add(urlToIcon);
-        add(processName);
-        add(excludeCli);
-        add(useCustomConfig);
-        add(compile);
+        add(author, grid(8,0,10,1));
+        add(title, grid(4,1,9,2));
+        add(compile, grid(11,16,4,1));
+        add(excludeCli, grid(1,16,5,1));
+
+        add(topBackground, grid(0,0,16,4));
+        add(cmdBackground, grid(0,18,16,4));
+        add(centerBackground, grid(0,0,16,22));
+
         pack();
         setLocationRelativeTo(null);
+    }
+
+    private GridBagConstraints grid(int x, int y){
+        locator.gridx = x;
+        locator.gridy = y;
+        locator.gridwidth = 1;
+        locator.gridheight = 1;
+        return locator;
+    }
+
+    private GridBagConstraints grid(int x, int y, int width, int height){
+        locator.gridx = x;
+        locator.gridy = y;
+        locator.gridwidth = width;
+        locator.gridheight = height;
+        return locator;
     }
 }
